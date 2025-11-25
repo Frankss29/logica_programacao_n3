b131c7028e04f103965ec82c2a5972ee4ea0a0d0
FRANCIELE SANTOS DE JESUS <franciele.sjesus2@senacsp.edu.br>
2025-11-24 21:44:59 -0300

    Aula 10 Exercícios Gerais

---
+++ Aula10-ExerciciosGeral/Program.cs
@@ -0,0 +1,48 @@
+﻿//EXERCÍCIOS - AULA 10
+
+//1)Crie um programa que peça ao usuário uma senha. O programa deve permitir até 3 tentativas. 
+//A senha correta é "12345". 
+//Se o usuário digitar a senha correta E tiver mais de 18 anos (idade informada previamente), o acesso é liberado. 
+//Caso contrário, o programa informa que o acesso foi negado. 
+
+const int QTD_TENTATIVA=3;
+string entradaUser;
+int senhaValida = 12345;
+
+Console.WriteLine(" Informe sua idade: ");
+entradaUser = Console.ReadLine();
+int.TryParse(entradaUser, out int idade);
+
+if (idade >= 18)
+{
+    Console.WriteLine(" Informe a senha: ");
+    entradaUser = Console.ReadLine();
+    int.TryParse(entradaUser, out int senha);
+
+    if (senha == senhaValida)
+    {
+
+        Console.WriteLine("ACESSO LIBERADO");
+        Console.WriteLine("----------------");
+        Console.WriteLine("Bem-Vindo!!");
+
+    }
+    else if (senha != senhaValida)
+    {
+
+        for (int contador = 0; contador < 3; contador++)
+        {
+
+            Console.WriteLine(" Senha incorreta informe novamente: ");
+        }
+
+    }
+}else{
+    Console.WriteLine("VOLTE QUANDO TIVER 18 ANOS");
+}
+
+
+
+
+
+
--- Aula9-LaçosDeRepetição/Program.cs
+++ Aula9-LaçosDeRepetição/Program.cs
@@ -137,22 +137,69 @@
 //    count++;
 //}
 
+
 //2)Validação de Nota: Faça um programa que peça uma nota entre zero e dez.
 //Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
 
 
-int nota;
+//Console.WriteLine("Digite uma Nota: ");
+//string entradaUser = Console.ReadLine();
+//bool valido = int.TryParse(entradaUser, out int nota);//Acrescentei com a correção do Professor
 
-Console.WriteLine("Digite uma Nota: ");
-nota = int.Parse(Console.ReadLine());
+////DOIS PAPE QUE DIZER OU EXEMPLO "NOTA MENOR QUE ZERO OU NOTA MAIOR QUE 10) OU É UM OPERADOR LOGICO
 
-for(int count=0;nota>10; count++)
+//while (!valido || nota<0 || nota > 10)
 
+//{
+
+//    Console.WriteLine("Nota inválida digite novamente: ");
+//    nota = int.Parse(Console.ReadLine());
+
+//}
+
+//Console.WriteLine("\n-----\n");
+
+//3) Controle de peso: Faça um programa que receba a idade e o peso de sete pessoas. Calcule e mostre:  
+//A quantidade de pessoas com mais de 90 quilos;
+//A média das idades das sete pessoas;
+
+
+////DECLARAÇÃO DE VARIAVEIS
+int peso;
+int idade;
+// Variaes para guardar os valores pedidos no console
+int somaDasIdades = 0;
+int pessoasMais90Kg = 0;
+
+for (int count = 1; count <= 7; count++)
 {
 
-    Console.WriteLine("Nota inválida digite novamente: ");
-    
+    Console.Write("Informe a Idade: ");
+    idade = int.Parse(Console.ReadLine());
+    somaDasIdades = somaDasIdades + idade;
+    Console.Write("Informe o Peso (Kg): ");
+    peso = int.Parse(Console.ReadLine());
+
+    //Verificar se o peso +90
+    if (peso > 90)
+    {
+        pessoasMais90Kg = pessoasMais90Kg + 1;
+    }
+    Console.WriteLine("\n-----\n");
 }
+
+// 3. RESULTADOS (Após o loop)
+
+double mediaIdades = somaDasIdades / 7;
+
+Console.WriteLine("Quantidade de pessoas com mais de 90 Kg: " + pessoasMais90Kg);
+
+Console.WriteLine(" Média das idades das 7 pessoas: " + mediaIdades);
+
+
+// LEMBRA NOS PROXIMOS EXERCICIOS DE  VER SE O QUE FOI DIGITADO É VALIDO
+//bool valido= int.TryParse(entradaUsuario, out int nota);
+
 
 
 
