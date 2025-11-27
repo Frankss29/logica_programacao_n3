96e7c6be30c515782c8e2396d7855b9c01e5da1e
FRANCIELE SANTOS DE JESUS <franciele.sjesus2@senacsp.edu.br>
2025-11-26 21:37:38 -0300

    Aula 11 - Array|Vetor|List

--- Aula10-ExerciciosGeral/Program.cs
+++ Aula10-ExerciciosGeral/Program.cs
@@ -5,44 +5,123 @@
 //Se o usuário digitar a senha correta E tiver mais de 18 anos (idade informada previamente), o acesso é liberado. 
 //Caso contrário, o programa informa que o acesso foi negado. 
 
-const int QTD_TENTATIVA=3;
-string entradaUser;
-int senhaValida = 12345;
+//const int QTD_TENTATIVA=3;
+//const int senhaValida = 12345;
+//string entradaUser;
 
-Console.WriteLine(" Informe sua idade: ");
-entradaUser = Console.ReadLine();
-int.TryParse(entradaUser, out int idade);
+//Console.WriteLine(" Informe sua idade: ");
+//entradaUser = Console.ReadLine();
+//int.TryParse(entradaUser, out int idade);
 
-if (idade >= 18)
+//if (idade >= 18)
+//{
+//    Console.WriteLine(" Informe a senha: ");
+//    entradaUser = Console.ReadLine();
+//    int.TryParse(entradaUser, out int senha);
+
+//    if (senha == senhaValida)
+//    {
+
+//        Console.WriteLine("ACESSO LIBERADO");
+//        Console.WriteLine("----------------");
+//        Console.WriteLine("Bem-Vindo!!");
+
+//    }
+//    else 
+//    {
+//        int tentativaRestantes = QTD_TENTATIVA - 1;
+
+//        while(senha != senhaValida && tentativaRestantes > 0 )
+//        {
+
+//            Console.WriteLine(" Senha incorreta informe novamente: ");
+//            entradaUser = Console.ReadLine();
+//            int.TryParse(entradaUser, out senha);
+//            tentativaRestantes--;  
+
+//        }if (senha==senhaValida)
+//        {
+//            Console.WriteLine("ACESSO LIBERADO");
+//            Console.WriteLine("----------------");
+//            Console.WriteLine("Bem-Vindo!!");
+
+//        }
+//        else
+//        {
+//            Console.WriteLine("ACESSO NEGADO VOCÊ EXCEDEU NÚMEROS DE TENTATIVAS DE SENHA ");
+//        }
+
+//    }
+//}else{
+//    Console.WriteLine("VOLTE QUANDO TIVER 18 ANOS");
+//}
+
+//Exercício 2 – Números Pares e Ímpares 
+//Peça ao usuário um número inteiro positivo. O programa deve imprimir todos os números de 1 até esse número. 
+//Para cada número, informe se ele é par ou ímpar. 
+//Se o número for par e múltiplo de 5 ao mesmo tempo, destaque com a mensagem "Par e múltiplo de 5". 
+
+
+//Console.WriteLine("Insirar um número inteiro e positivo: ");
+//bool inteiro = int.TryParse(Console.ReadLine(), out int numero);
+
+//if (inteiro && numero > 0)
+//{
+//    for (int contador = 1; contador <= numero; contador++)
+//    {
+//        bool par = contador % 2 == 0;
+//        bool multiplo5 = contador % 5 == 0;
+
+//        if (par && multiplo5)
+//        {
+//            Console.WriteLine(contador + " é par e múltiplo de 5. ");
+//        }
+//        else if (par && !multiplo5)
+//        {
+//            Console.WriteLine(contador + " é par.");
+//        }
+//        else
+//        {
+//            Console.WriteLine(contador + " é impar.");
+//        }
+
+//    }
+// }
+//else
+//{
+//    Console.WriteLine("O número informado não é válido");
+//}
+
+
+//Exercício 3 – Caixa Registradora 
+//Simule uma caixa registradora simples. 
+//O programa deve pedir o valor de cada produto até que o usuário digite 0 para encerrar. 
+//Ao final, mostre o total da compra. 
+//Se o total for maior que R$ 100 OU o cliente tiver um cupom de desconto
+//(informado como CUPOM10), aplique 10% de desconto. 
+
+const string CUPOM10 = "CUPOM10";
+double valor = -1;
+double total = 0;
+
+while (valor != 0)
 {
-    Console.WriteLine(" Informe a senha: ");
-    entradaUser = Console.ReadLine();
-    int.TryParse(entradaUser, out int senha);
-
-    if (senha == senhaValida)
+    Console.WriteLine("Insira o valor do produto:");
+    valor = double.Parse(Console.ReadLine());
+    if (valor > 0)
     {
-
-        Console.WriteLine("ACESSO LIBERADO");
-        Console.WriteLine("----------------");
-        Console.WriteLine("Bem-Vindo!!");
-
+        total += valor;
     }
-    else if (senha != senhaValida)
-    {
-
-        for (int contador = 0; contador < 3; contador++)
-        {
-
-            Console.WriteLine(" Senha incorreta informe novamente: ");
-        }
-
-    }
-}else{
-    Console.WriteLine("VOLTE QUANDO TIVER 18 ANOS");
 }
 
+Console.WriteLine("Insira o cupom de desconto:");
+string cupom = Console.ReadLine();
 
-
-
-
-
+if (cupom == CUPOM10 || total > 100)
+{
+    Console.WriteLine("O valor total com 10% de desconto é R$ " + (total * 0.9));
+}
+else
+{
+    Console.WriteLine("O valor total é R$ " + total);
+}
---
+++ Aula11-ArraysVetor/Program.cs
@@ -0,0 +1,47 @@
+﻿//AULA 11 - ARRAYS | VETOR | LIST
+
+//int[] notas = new int[2];
+
+//Console.WriteLine("Nota p1:");
+//notas[0] = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Nota p2:");
+//notas[1] = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Nota final: " + ((notas[0] + notas[1]) / 2));
+
+//List<int> notas = new List<int>();
+
+//Console.WriteLine("Nota p1:");
+//notas.Add(int.Parse(Console.ReadLine()));
+
+//Console.WriteLine("Nota p2:");
+//notas.Add(int.Parse(Console.ReadLine()));
+
+//Console.WriteLine("Nota final: " + ((notas[0] + notas[1]) / 2));
+
+//for (int i = 0; i < notas.Count; i++)
+//{
+//    Console.WriteLine(notas[i]);
+//}
+
+
+//FILTRO DE NÚMEROS PARES 
+
+Random random = new Random();
+//List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+List<int> numeros = new();
+
+for (int i = 0; i < random.Next(1, 10001); i++)
+{
+    numeros.Add(random.Next(1, 101));
+}
+
+for (int i = 0; i < numeros.Count; i++)
+{
+    int numero = numeros[i];
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine(numero);
+    }
+}
