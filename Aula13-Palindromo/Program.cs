adef124d11a7feb5ef8ba4d2b460baa0ec575190
FRANCIELE SANTOS DE JESUS <franciele.sjesus2@senacsp.edu.br>
2025-12-03 21:45:22 -0300

    Aula 13 -Exercício Palindro

---
+++ Aula13-Palindromo/Program.cs
@@ -0,0 +1,20 @@
+﻿//Exercício Palindromo - Aula 13
+//Exemplos de palindromo Arara, Ovo etc
+
+Console.WriteLine("Insira uma palavra: ");
+string palavra = Console.ReadLine().ToLower();
+string palavraInvertida = "";
+
+for (int i = palavra.Length -1; i >= 0; i--)
+{
+    palavraInvertida += palavra[i];
+}
+
+if (palavraInvertida == palavra)
+{
+    Console.WriteLine("PALINDROMO!");
+}
+else
+{
+    Console.WriteLine("é só uma palavra mesmo!");
+}
